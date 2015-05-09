659c281115170865c99be8a52c81afc4d0171a8d
Lawrence Sebald <ljsebald@users.sourceforge.net>
2015-05-08 22:36:53 -0400

    Fix compilation of libgl.

--- libgl/files/KOSMakefile.mk
+++ libgl/files/KOSMakefile.mk
@@ -3,6 +3,6 @@ OBJS =  gl-rgb.o gl-fog.o gl-sh4-light.o gl-light.o gl-clip.o gl-clip-arrays.o
 OBJS += gl-arrays.o gl-pvr.o gl-matrix.o gl-api.o gl-texture.o glu-texture.o
 OBJS += gl-framebuffer.o gl-cap.o gl-error.o
 
-KOS_CFLAGS += -Iinclude -ffast-math -O3
+KOS_CFLAGS += -Iinclude -ffast-math -O3 -DBUILD_LIBGL
 
 include ${KOS_PORTS}/scripts/lib.mk
