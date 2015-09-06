aea43f31d8bcea01bd9151a5d9fbb7cd8df08c00
Lawrence Sebald <ljsebald@users.sourceforge.net>
2015-09-06 16:29:11 -0400

    Rename libgl -> libGL.

--- libgl/files/KOSMakefile.mk
+++ libgl/files/KOSMakefile.mk
@@ -1,4 +1,4 @@
-TARGET = libgl.a
+TARGET = libGL.a
 OBJS =  gl-rgb.o gl-fog.o gl-sh4-light.o gl-light.o gl-clip.o gl-clip-arrays.o
 OBJS += gl-arrays.o gl-pvr.o gl-matrix.o gl-api.o gl-texture.o glu-texture.o
 OBJS += gl-framebuffer.o gl-cap.o gl-error.o
