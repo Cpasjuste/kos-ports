78db216962b3b16ca5bbaf6d4d38dbd3ed22c16d
cpasjuste <cpasjuste@gmail.com>
2021-03-02 18:46:35 +0100

    gldc: revert before yalloc

--- GLdc/files/KOSMakefile.mk
+++ GLdc/files/KOSMakefile.mk
@@ -2,7 +2,6 @@ TARGET = libGLdc.a
 
 OBJS = GL/draw.o GL/flush.o GL/framebuffer.o GL/immediate.o GL/lighting.o GL/state.o GL/texture.o GL/glu.o
 OBJS += GL/matrix.o GL/fog.o GL/error.o GL/clip.o containers/stack.o containers/named_array.o containers/aligned_vector.o GL/profiler.o
-OBJS += GL/yalloc/yalloc.o
 
 KOS_CFLAGS += -ffast-math -Ofast -Iinclude
 
