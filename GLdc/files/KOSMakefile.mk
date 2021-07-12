a84b6b454734918a448a8d84a8c2ef4412e7e331
cpasjuste <cpasjuste@gmail.com>
2021-07-12 15:14:29 +0200

    GLdc: fix version, freetype: cleanup

--- GLdc/files/KOSMakefile.mk
+++ GLdc/files/KOSMakefile.mk
@@ -4,14 +4,14 @@ OBJS = containers/aligned_vector.o containers/named_array.o containers/stack.o
 OBJS += GL/clip.o GL/draw.o GL/error.o GL/flush.o GL/fog.o GL/framebuffer.o
 OBJS += GL/glu.o GL/immediate.o GL/lighting.o GL/matrix.o GL/state.o GL/texture.o
 OBJS += GL/util.o GL/yalloc/yalloc.o
-OBJS += GL/platforms/sh4.o
+OBJS += GL/platforms/sh4.o GL/version.o
 
 KOS_CFLAGS += -O3 --fast-math -Iinclude -DBACKEND_KOSPVR
 
-defaultall: GL/version.h $(OBJS) subdirs linklib
+defaultall: GL/version.c $(OBJS) subdirs linklib
 
-GL/version.h:
+GL/version.c:
 	rm -f $@
-	@printf '#pragma once\n#define GLDC_VERSION "$(shell git describe --abbrev=4 --dirty --always --tags)"\n' > $@
+	@printf 'const char* GLDC_VERSION = "$(shell git describe --abbrev=4 --dirty --always --tags)";\n' > $@
 
 include ${KOS_PORTS}/scripts/lib.mk
